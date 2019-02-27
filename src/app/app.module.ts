002942d6ce03697a4f9e3cd4a01ed068967bda47
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-02-27 11:48:37 -0700

    Imported the nesseccary http components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FruitsComponent } from './fruits/fruits.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { FruitsComponent } from './fruits/fruits.component';
     FruitsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
