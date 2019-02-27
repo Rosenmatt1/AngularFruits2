9232ff433dd592b26ccf511e06c0f9cd28c5bf9b
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-02-27 12:03:14 -0700

    I had changed the selector in the app.component.ts and it broke the function.  Changed it back to app-root

--- src/app/fruits/fruits.component.ts
+++ src/app/fruits/fruits.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-fruits',
+  selector: 'fruit-loop',
   templateUrl: './fruits.component.html',
   styleUrls: ['./fruits.component.scss']
 })
